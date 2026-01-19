b91d6d816f86a6aacfb3e3d9dd925c91906d09bd
Priyankal52 <sajjanpriyanka15@gmail.com>
2026-01-19 23:45:05 +0530

    65554

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from dJango.contrib import admin
+from .models import MenuCategory
+admin.site.register(MenuCategory)
 
-# Create your models here.
+class MenuCategory(models.Model):
+    name= models.CharFeild(max_length=100,unique=True)
+    def __str__(self):
+        return self.name
