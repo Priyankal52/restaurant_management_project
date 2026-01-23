851a3bddc9ed9d7230c7266b93d3a91a9de5a266
Priyankal52 <sajjanpriyanka15@gmail.com>
2026-01-23 23:38:35 +0530

    65557

--- orders/models.py
+++ orders/models.py
@@ -22,3 +22,14 @@ class OrderStatus(models.model):
 
     def __str __(self):
         return self.name
+    
+    from django.db import models
+    class Coupon(models.MOdel):
+        code = models. CharField(max_length=50, unique=True)
+        discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+        is_active = models.BooleanField(default=True)
+        valid_from = models. DateField()
+        valid_until = models.DateField()
+
+        def __str __(self):
+            return self.code
