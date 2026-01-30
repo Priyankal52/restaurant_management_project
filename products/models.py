a2042779f6c17bb9305764c5569ac2458160d06f
Priyankal52 <sajjanpriyanka15@gmail.com>
2026-01-30 23:54:56 +0530

    65980

--- products/models.py
+++ products/models.py
@@ -1,10 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-class Item(models.Model):
-    item_name = models.CharField(max_length=150)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
+class order(models.model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,, related_name="orders")
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return str(self.item_name) 
+    total_price = models.DecimalField()
